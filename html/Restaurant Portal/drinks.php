fda3716d83ff0c470553ad489eba7c5bc534abc3
sanyaxx <sanyag2004@gmail.com>
2024-04-16 15:47:42 +0800

    changes

--- html/Restaurant Portal/drinks.php
+++ html/Restaurant Portal/drinks.php
@@ -32,7 +32,7 @@ if ($result->num_rows > 0) {
     mysqli_close($connection);
 
     // Now, include the HTML content from the appetizer.html file
-    include("menu.html");
+    include("updateStock.html");
 
 } else {
     echo "0 results";
